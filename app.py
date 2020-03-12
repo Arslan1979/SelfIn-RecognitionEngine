8978785e696cf87b0721103f755ac50848a5ce6e
AjithThanam <ajith.thanam@gmail.com>
2020-03-12 01:46:18 -0400

    Added image upload constraints

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-
+from werkzeug.utils import secure_filename
 from flask import Flask, jsonify, render_template, request, redirect, make_response
 app = Flask(__name__)
 import os
 
-#Location of where the images will be saved
+#Location of where the images will be saved, and types of images 
 app.config["IMAGE_UPLOADS"] = "images"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
 
 @app.route("/")
 def hello():
@@ -13,9 +14,20 @@ def hello():
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route("/more")
-def more():
-    return render_template("index.html")
+#Check if uploaded image is valid, via the extension
+def allowedImages(filename):
+
+    #ensure it has extension
+    if not "." in filename:  
+        return False
+    
+    #remove extension
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
 
 #Route to upload image
 @app.route("/upload", methods=["GET", "POST"])
@@ -25,9 +37,18 @@ def upload():
             #"image" is the name given to the input field in the html
             image = request.files["image"]
 
-            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
-            #flush is to prevent the print to get stuck in a buffer
-            print("image saved", flush=True)
-            return redirect(request.url)
+            if image.filename == "":
+                print("No Filename")
+                return redirect(request.url)
+            
+            #check if image is of right type and secure the name (removes path is associated with filename) if it is
+            if allowedImages(image.filename):
+                filename = secure_filename(image.filename)
+                print(filename, flush=True)
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+                return redirect(request.url)
+            else:
+                print("The file extension is not allowed")
+                return redirect(request.url)
 
     return render_template("upload_image.html")
