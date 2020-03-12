43a97a5b289499131a8839edbf0122ffc260371b
AjithThanam <ajith.thanam@gmail.com>
2020-03-12 01:27:43 -0400

    Implemented image storing

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+
 from flask import Flask, jsonify, render_template, request, redirect, make_response
 app = Flask(__name__)
+import os
+
+#Location of where the images will be saved
+app.config["IMAGE_UPLOADS"] = "images"
 
 @app.route("/")
 def hello():
@@ -19,8 +24,10 @@ def upload():
         if request.files:
             #"image" is the name given to the input field in the html
             image = request.files["image"]
+
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
             #flush is to prevent the print to get stuck in a buffer
-            print(image, flush=True)
+            print("image saved", flush=True)
             return redirect(request.url)
 
     return render_template("upload_image.html")
