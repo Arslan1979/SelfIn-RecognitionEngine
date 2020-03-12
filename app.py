856e86cda06c045bda41ffadfc590fd7b344dbcc
AjithThanam <ajith.thanam@gmail.com>
2020-03-12 01:03:13 -0400

    Route for uploading images

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, redirect, make_response
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,3 +7,20 @@ def hello():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route("/more")
+def more():
+    return render_template("index.html")
+
+#Route to upload image
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        if request.files:
+            #"image" is the name given to the input field in the html
+            image = request.files["image"]
+            #flush is to prevent the print to get stuck in a buffer
+            print(image, flush=True)
+            return redirect(request.url)
+
+    return render_template("upload_image.html")
